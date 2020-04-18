d3fd498381fd1ed72c22571ce499a98144def0e2
imKashyap <rahulkashyap4041@gmail.com>
2020-04-18 20:12:25 +0530

    Adding initial files

---
+++ lib/game_controller.dart
@@ -0,0 +1,30 @@
+import 'dart:ui';
+import 'package:breakbricks/player.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+
+class GameController extends Game{
+  Size screenSize;
+  double tileSize;
+  Player player;
+  GameController(){
+    initialize();
+  }
+  void initialize() async{
+    resize(await Flame.util.initialDimensions());
+    player=Player(this);
+  }
+  void render(Canvas c){
+    Rect background=Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
+    Paint backgroundPaint=Paint()..color=Color(0xFFFAFAFA);
+    c.drawRect(background, backgroundPaint);
+    player.render(c);
+  }
+  void update(double t){}
+  void resize(Size size){
+    screenSize=size;
+    tileSize=screenSize.width/10;
+  }
+
+
+}
